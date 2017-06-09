e513631c9fdefe38286a803dd6baeffb914f010f
Henrich Horváth <dev@henrich-horvath.eu>
2017-06-09 15:08:26 +0200

    example

--- src/uib/leapmotion/darts/Main.java
+++ src/uib/leapmotion/darts/Main.java
@@ -1,15 +1,26 @@
 package uib.leapmotion.darts;
 
-import com.leapmotion.leap.*;
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Gesture;
+
+import java.io.IOException;
 
 class Main {
-    public static void Main(String[] args) {
-
-        // Keep this process running until Enter is pressed
+    public static void main(String[] args)
+    {
+        Controller controller = new Controller();
+        controller.setPolicyFlags(Controller.PolicyFlag.POLICY_BACKGROUND_FRAMES);
+        SampleListener listener = new SampleListener();
+        controller.addListener(listener);
+        // controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
+        // controller.enableGesture(Gesture.Type.TYPE_SWIPE);
+        controller.enableGesture(Gesture.Type.TYPE_CIRCLE);
         System.out.println("Press Enter to quit...");
         try {
             System.in.read();
-        } catch (Exception e) {
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        controller.removeListener(listener);
     }
 }
