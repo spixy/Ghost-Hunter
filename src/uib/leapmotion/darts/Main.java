a33e00a1b8a3db7139c11883411f85aaa1baf90e
Henrich Horváth <dev@henrich-horvath.eu>
2017-06-09 14:50:56 +0200

    initial IDEA project structure

---
+++ src/uib/leapmotion/darts/Main.java
@@ -0,0 +1,15 @@
+package uib.leapmotion.darts;
+
+import com.leapmotion.leap.*;
+
+class Main {
+    public static void Main(String[] args) {
+
+        // Keep this process running until Enter is pressed
+        System.out.println("Press Enter to quit...");
+        try {
+            System.in.read();
+        } catch (Exception e) {
+        }
+    }
+}
