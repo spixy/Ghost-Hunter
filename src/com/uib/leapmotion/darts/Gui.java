267cc7bc75ca181fe9c4ec4cbb073a18359f75f6
Henrich Horváth <dev@henrich-horvath.eu>
2017-06-09 19:11:35 +0200

    refactoring

--- src/com/uib/leapmotion/darts/Gui.java
+++ src/com/uib/leapmotion/darts/Gui.java
@@ -90,13 +90,7 @@ public class Gui extends JFrame {
     private void DrawUpdate() {
         Graphics2D g2d = graphicsContext.createGraphics();
         g2d.setRenderingHints(antialiasing);
-
         g2d.clearRect(0, 0, getWidth(), getHeight());
-
-        //Set up the font to print on the circles
-        Font font = g2d.getFont();
-        font = font.deriveFont(Font.BOLD, 14f);
-        g2d.setFont(font);
 
         // spawn new enemy
         if (enemies.size() < MaxEnemyCount && random.nextFloat() < enemySpawnProbability)
@@ -110,19 +104,30 @@ public class Gui extends JFrame {
             enemy.Draw(g2d);
         }
 
-        float radius = dartRadius * dartZ;
-
-        //set up the large circle
-        Ellipse2D.Double dart = new Ellipse2D.Double(dartX - radius, dartY - radius, 2 * radius, 2 * radius);
-
-        //we save the big circle for last, to cover up any stray marks under the stroke
-        //of its perimeter. We also set the clip back to null to prevent the large circle
-        //itself from accidentally getting clipped
-        g2d.setColor(dartColor);
-        g2d.draw(dart);
+        this.DrawDart(g2d);
+        this.DrawUI(g2d);
 
         g2d.dispose();
         //force the container for the context to re-paint itself
         contextRender.repaint();
     }
+
+    private void DrawDart(Graphics2D g2d) {
+        float radius = dartRadius * dartZ;
+        //set up the large circle
+        Ellipse2D.Double dart = new Ellipse2D.Double(dartX - radius, dartY - radius, 2 * radius, 2 * radius);
+        g2d.setColor(dartColor);
+        g2d.draw(dart);
+    }
+
+    private void DrawUI(Graphics2D g2d) {
+        //Set up the font to print on the circles
+        Font font = g2d.getFont();
+        font = font.deriveFont(Font.BOLD, 14f);
+        g2d.setFont(font);
+
+        g2d.setColor(Color.WHITE);
+        g2d.drawString("Score: " + "ScoreCounterPlaceholder", 5, 5);
+    }
+
 }
